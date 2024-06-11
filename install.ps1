8b25d5c92923db948e6f916646a01dd286da1fd7
Jaedan Calder <jcalder@hammond.com.au>
2024-06-11 14:58:54 +1000

    Updated setup.py in Nested. Using local venv for py2exe

--- install.ps1
+++ install.ps1
@@ -1,27 +1,41 @@
 python -m venv venv
 ./venv/Scripts/activate
-pip install -r requirements.txt
-
+pip install -q -r requirements.txt
 python compile.py -d
-pip install -r requirements_gen.txt
+
+pip install -q -r requirements_gen.txt
 git submodule update --recursive
 
 cd tools
 
 cd LoudPing
+python -m venv venv
+./venv/Scripts/activate
+pip install -q -r requirements.txt
 python setup.py
 cd ..
 
 cd Meraki-App
+python -m venv venv
+./venv/Scripts/activate
+pip install -q -r requirements.txt
 python setup.py
 cd ..
 
 cd MultiPing
+python -m venv venv
+./venv/Scripts/activate
+pip install -q -r requirements.txt
 python setup.py
 cd ..
 
 cd Ping-App
+python -m venv venv
+./venv/Scripts/activate
+pip install -q -r requirements.txt
 python setup.py
 cd ..
 
 cd ..
+
+./venv/Scripts/activate
