407f801169c9c38d823a328cc40fd246b5c369cb
Jaedan Calder <jcalder@hammond.com.au>
2024-10-03 11:26:51 +1000

    Bumped to v1.91.2

--- compile.py
+++ compile.py
@@ -11,7 +11,7 @@ def create_file_if_not_exist(path, contents=""):
     if os.path.exists(path):
         return
 
-    with open(path, "a") as f:
+    with open(path, "a", encoding="utf-8") as f:
         f.write(contents)
 
 
@@ -19,14 +19,14 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python compile.py [-d] | [pygui_directory]")
         return
-    
+
     if "-d" in sys.argv:
         # https://docs.github.com/en/rest/releases/releases
         print("Querying Github API for latest pygui release")
         res = APIRequest("https://api.github.com/repos/JaedanC/pygui/releases/latest") \
             .set_method(Method.Get) \
             .execute()
-        
+
         asset = res.json_dict()["assets"][0]
         filename: str = asset["name"]
         download_url: str = asset["browser_download_url"]
@@ -36,22 +36,22 @@ def main():
             .set_method(Method.Get) \
             .execute() \
             .raw()
-        
+
         zip_name = "downloads/" + filename
         folder_name = zip_name.removesuffix(".zip")
 
         with safe_open_wb(zip_name) as f:
             f.write(raw_res.content)
-        
+
         print(f"Unzipping to {folder_name}")
         with zipfile.ZipFile(zip_name) as zip_f:
             zip_f.extractall(folder_name)
-        
+
         pygui_release_dir = folder_name
         print(pygui_release_dir)
     else:
         pygui_release_dir = sys.argv[1]
-    
+
     pygui_folder = os.path.join(pygui_release_dir, "pygui")
     pygui_demo = os.path.join(pygui_release_dir, "pygui_demo.py")
 
@@ -108,15 +108,15 @@ def main():
     for requirements_file in requirements_files:
         if not os.path.exists(requirements_file):
             continue
-    
-        with open(requirements_file) as f:
+
+        with open(requirements_file, encoding="utf-8") as f:
             dependencies += f.readlines()
-    
+
     dependencies = list(map(lambda x: x.strip(), dependencies))
     dependencies = list(set(dependencies))
     dependencies.sort()
 
-    with open("requirements_gen.txt", "w") as f:
+    with open("requirements_gen.txt", "w", encoding="utf-8") as f:
         for dependency in dependencies:
             dependency = dependency.strip()
             if dependency == "":
@@ -124,11 +124,11 @@ def main():
 
             f.write(dependency)
             f.write("\n")
-    
+
     print("Creating binary folder")
     if not os.path.exists("bin"):
         os.mkdir("bin")
-    
+
     bat_src = \
     textwrap.dedent("""
     @echo off
@@ -169,7 +169,7 @@ def main():
     }]
 
     for bin_detail in bin_details:
-        with open(os.path.join(binary_dir, bin_detail["bat"]), "w") as f:
+        with open(os.path.join(binary_dir, bin_detail["bat"]), "w", encoding="utf-8") as f:
             f.write(bat_src.format(
                 working_dir=os.path.join(base_absolute, "tools", bin_detail["dir"], "dist"),
                 exe_name=bin_detail["exe"]
